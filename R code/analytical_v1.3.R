f82ed71c48747b6845ab4aee329e424818550ce7
Matt Boyas <boyasm@msn.com>
2013-12-08 14:10:24 -0800

    Broke testing into its own function.

--- R code/analytical_v1.3.R
+++ R code/analytical_v1.3.R
@@ -1,29 +1,31 @@
 #Matt Boyas, Michelle Newcomer, Ying Chao Shi
-#Code Version 12/07/2013
+#Code Version 12/08/2013
 #Known Bugs: None
 
 set.seed(0)
+#only set the seed if you are trying to replicate algorithmic output
 
 #------Begin User input-----------------------------
 density <- quote((1/sqrt(2*pi))*exp((-x^2)/2))
-xabs<-c(-1,1) #user specified starting points
-accept<-5000 #total number of points required to accept
+xabs <- c(-1,1) #user-specified starting abcissae
+accept <- 5000 #total number of points required to accept
+
 #Make sure to initialize the function "ARS" before running line 12 
-ARS_values <- ARS(density,xabs,accept) #run this line to run the entire sampling scheme
+ARS_values <- ARS(density, xabs, accept) #run this line to run the entire sampling scheme
 #-----End user Input--------------------------------
 
 
 
 #------Begin ARS Scheme-----------------------------
 
-ARS <- function(density,xabs,accept){
+ARS <- function(density, xabs, accept, endpoints=c(-Inf, Inf)){
   
   #------Prep Function to define h(x) and h'(x)-----------------------------
   
   prep <- function(f){
-    logQuote <- bquote(log(.(density)))
-    deriv<- D(logQuote, "x")
-    return(list(logQuote, deriv))
+    log_quote <- bquote(log(.(density)))
+    deriv <- D(log_quote, "x")
+    return(list(log_quote, deriv))
   }
   #compute the log and the derivative of the log in a preparation function
   
@@ -33,14 +35,14 @@ ARS <- function(density,xabs,accept){
     return(eval(prep[[1]]))
   }
   
-  hPrime <- function(x){
+  h_prime <- function(x){
     return(eval(prep[[2]]))
   }
   
-  #h and hPrime take x as the sole input and can be used in the computation of the equations in the paper algorithm
+  #h and h_prime take x as the sole input and can be used in the computation of the equations in the paper algorithm
   #------End Prep Function to define h(x) and h'(x)-----------------------------
   
-  
+
   #------Begin Functions for z and lower bound-----------------------------
   #Vectorized equation 1
   
@@ -48,7 +50,7 @@ ARS <- function(density,xabs,accept){
   zrange <- function(xabs){
     k <- length(xabs)
     j <- 1:(k-1)
-    z[j] <- (h(xabs[j+1])-h(xabs[j])-xabs[j+1]*hPrime(xabs[j+1])+xabs[j]*hPrime(xabs[j]))/(hPrime(xabs[j])-hPrime(xabs[j+1]))
+    z[j] <- (h(xabs[j+1])-h(xabs[j])-xabs[j+1]*h_prime(xabs[j+1])+xabs[j]*h_prime(xabs[j]))/(h_prime(xabs[j])-h_prime(xabs[j+1]))
     return(z)
   }
   
@@ -59,7 +61,6 @@ ARS <- function(density,xabs,accept){
   }
   #------End Functions for z and lower bound-----------------------------
   
-  
   #------Begin Sampling-----------------------------
   sample <- c()
   
@@ -68,17 +69,17 @@ ARS <- function(density,xabs,accept){
     
     #The end points of the upper bound.
     zvalue <- zrange(xabs)
-    interval<-c(-Inf,zvalue,Inf) #vector of the intervals for x
-    segment<-c(interval,xabs) #vector of the all the points
-    segment<-sort(segment)#sorted
+    interval <- c(endpoints[1], zvalue, endpoints[2]) #vector of the intervals for x
+    segment <- c(interval,xabs) #vector of the all the points
+    segment <- sort(segment)#sorted
     
     #The probabilities under each piece.
     prob <- c()
     for (i in 1:k){
-      uk_x<-function(x){
-        exp(h(xabs[i])+(x-xabs[i])*hPrime(xabs[i]))
+      uk_x <- function(x){
+        exp(h(xabs[i])+(x-xabs[i])*h_prime(xabs[i]))
       }
-      prob[i]<-integrate(uk_x,interval[i],interval[i+1])$value
+      prob[i] <- integrate(uk_x,interval[i],interval[i+1])$value
     }
     
     #Scale these probabilities so that they add up to 1 (because the upper bound is a pdf).
@@ -97,10 +98,10 @@ ARS <- function(density,xabs,accept){
       remain_prob=u-prob_cumulative[n-1]
     }
     #Second, integrate from the nth z to x*, set this integral to the remaining probablity, and solve x*. (Don't spend time reading this mess. We can go over it together when we meet :)
-    x_star=log(sum(prob)*hPrime(xabs[n])*remain_prob/exp(h(xabs[n])-hPrime(xabs[n])*xabs[n])+exp(hPrime(xabs[n])*segment[2*n-1]))/hPrime(xabs[n])
+    x_star=log(sum(prob)*h_prime(xabs[n])*remain_prob/exp(h(xabs[n])-h_prime(xabs[n])*xabs[n])+exp(h_prime(xabs[n])*segment[2*n-1]))/h_prime(xabs[n])
     
     ###Now that we have the x*, just calculate the upper and lower bound.
-    upper=exp(h(xabs[n])+(x_star-xabs[n])*hPrime(xabs[n]))
+    upper=exp(h(xabs[n])+(x_star-xabs[n])*h_prime(xabs[n]))
     #Remember we discussed that x* can fall into the two "tails". The paper saids if it does, then lower bound is -Inf.
     lower_segment=sort(c(-Inf,xabs,Inf))
     if(x_star<=xabs[1] || x_star>=xabs[k]){
@@ -133,31 +134,46 @@ ARS <- function(density,xabs,accept){
     
   }
   #------End Sampling-----------------------------
-
-  #------Begin Result-----------------------------
-
-  ##ploting for testing
-  par(mfrow=c(2,2))
-  #True standard normal
-  x=seq(-3,3,by=0.01)
-  plot(x,dnorm(x),type="l")
-  #Histogram of a sample from rnorm()
-  hist(rnorm(5000),freq=F)
-  #Histogram of a sample from our function
-  hist(sample,freq=F)
-  
-  ###Kolmogorov-Smirnov test for equality of two cdf distributions
-  KS <- ks.test(sample, "pnorm", 3, 2) # two-sided, exact
-  plot(sample,pnorm(sample, mean = 0, sd = 1, lower.tail = TRUE, log.p = FALSE),lty=1,col="red")
-  lines(x,pnorm(x, mean = 0, sd = 1, lower.tail = TRUE, log.p = FALSE),lty=1,col="blue")
-  
-  ###Report the pvalue
-  if(KS[[2]]<0.001){print("P-value < 0.001. ARS complete")}
-    else{print("P-value does not indicate statistical equality between the distributions")}
-  print(KS)
-  #------End Result-----------------------------
-  return(list(sample,xabs,KS))
-  
+return(sample)
 }
 #------End ARS Scheme-----------------------------
 
+ARS_testing <- function(sample){
+  #------Begin Result-----------------------------
+  
+  ##user visual plot for testing
+  
+  par(mfrow=c(3,1))
+  #True standard normal
+  x=seq(-3,3,by=0.01)
+  plot(x,dnorm(x), type="l", xlim=c(-4, 4), ylim=c(0, 0.4), main="Standard Normal PDF")
+  #Histogram of a sample from rnorm()
+  hist(rnorm(5000), freq=F, xlim=c(-4, 4), ylim=c(0, 0.4))
+  #Histogram of a sample from our function
+  hist(sample, freq=F, xlim=c(-4, 4), ylim=c(0, 0.4), main="Histogram of ARS sample")
+  input <- readline("Press <y> if these plots look correct.  Press any other key to stop the testing script.")
+  if (length(grep("y", input, ignore.case=TRUE))==0) {
+    print("User determined that plots do not match.  Terminating testing algorithm.")
+    stop(call. = FALSE)
+  }
+  else{
+    print("User determined that plots do match")
+    print("Beginning Kolmogorov-Smirnov test on Standard Normal")
+  
+    ###Kolmogorov-Smirnov test for equality of two cdf distributions
+    KS <- ks.test(sample, "pnorm", 3, 2) # two-sided, exact
+    par(mfrow=c(1,1))
+    plot(sample,pnorm(sample, mean = 0, sd = 1, lower.tail = TRUE, log.p = FALSE),lty=1,col="red")
+    lines(x,pnorm(x, mean = 0, sd = 1, lower.tail = TRUE, log.p = FALSE),lty=1,col="blue")
+  
+    ###Report the pvalue
+    if(KS[[2]]<0.001){print("P-value < 0.001. K-S test passed.")}
+    else{print("P-value does not indicate statistical equality between the distributions")}
+    print(KS)
+  #------End Result-----------------------------
+}
+}
+
+
+#lognormal
+density <- quote( (3*2^3)/(x^(3+1)) )
