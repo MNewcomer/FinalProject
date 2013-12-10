50b5ad8786ec23501147bca7b691190791acadb1
Matt Boyas <boyasm@msn.com>
2013-12-10 15:09:41 -0800

    Created code v1.6 to fix detecting non logconcave and bolstered testing script.

--- R code/analytical_v1.6.R
+++ R code/analytical_v1.6.R
@@ -12,21 +12,18 @@ density <- quote((1/sqrt(2*pi))*exp((-x^2)/2))
 xabs <- c(-1,1) #user-specified starting abcissae
 accept <- 1000 #total number of points required to accept
 ARS_values <- ARS(density, xabs, accept) #run this line to run the entire sampling scheme
-ARS_testing(ARS_values,density)
 
 #N(2,2)
 density1 <- quote((1/sqrt(2*pi*4))*exp(-(x-2)^2/(2*4)))
 xabs1 <- c(1,3)
 accept <- 1000 
 ARS_values1 <- ARS(density1,xabs1,accept)
-ARS_testing(ARS_values1,density1)
 
 #exp(0.5)
 density2 <- quote(0.5*exp(-0.5*x))
 xabs2 <- c(1,3)
 accept <- 1000
 ARS_values2 <- ARS(density2,xabs2,accept)
-ARS_testing(ARS_values2,density2)
 
 #Gamma(5,1)
 density3 <- quote(1/24*x^4*exp(-x))
@@ -34,7 +31,6 @@ xabs3 <- c(2,6)
 accept <- 1000
 endpoints3 <- c(0,Inf)
 ARS_values3 <- ARS(density3,xabs3,accept,endpoints3)
-ARS_testing(ARS_values3,density3)
 
 #Beta(2,2)
 density4 <- quote(6*x*(1-x))
@@ -42,13 +38,13 @@ xabs4 <- c(0.3,0.7)
 accept <- 1000 
 endpoints4 <- c(0,1)
 ARS_values4 <- ARS(density4,xabs4,accept,endpoints4)
-ARS_testing(ARS_values4,density4)
 
 #Pareto(2,3)
 density5 <- quote((3*2^3)/x^(3+1))
 xabs5 <- c(2, 5)
 accept <- 1000
 endpoints5 <- c(1,Inf)
+ARS_values5 <- ARS(density5,xabs5,accept,endpoints5)
 
 #-----End user Input--------------------------------
 
@@ -57,6 +53,14 @@ endpoints5 <- c(1,Inf)
 #------Begin ARS Scheme-----------------------------
 
 ARS <- function(density, xabs, accept, endpoints=c(-Inf, Inf)){
+  
+  #--------Check to make sure abcissae are within the density endpoints------
+  if (xabs[1]<endpoints[1] | xabs[2]>endpoints[2]){
+    return("Initial abcissae are not in the density's specified domain.  Terminating ARS algorithm.")
+  }
+  #--------End check to make sure abcissae are within the density endpoints------
+  
+  
   
   #------Prep Function to define h(x) and h'(x)-----------------------------
   
@@ -122,6 +126,7 @@ ARS <- function(density, xabs, accept, endpoints=c(-Inf, Inf)){
  	     uk_x <- function(x){
  	       exp(h(xabs[i])+(x-xabs[i])*h_prime(xabs[i]))
  	     }
+
  	     prob[i] <- integrate(uk_x,interval[i],interval[i+1])$value
  	   }
  	   
@@ -153,7 +158,11 @@ ARS <- function(density, xabs, accept, endpoints=c(-Inf, Inf)){
 		m=sum(x_star>xabs) 
 	      lower=exp(((xabs[m+1]-x_star)*h(xabs[m])+(x_star-xabs[m])*h(xabs[m+1]))/(xabs[m+1]-xabs[m]))
 	    }
-	    
+      
+      #Check for log concavity before moving forward
+      if (upper<exp(h(x_star)) | lower>exp(x_star)){
+ 	     return("The density function is not logarithmically concave.  Terminating ARS algorithm.")}
+      
     	###Either put x* into the sample or add it as another tangent point.
    	 #Generate w from uniform distribution.
    	 w=runif(1)
@@ -166,15 +175,6 @@ ARS <- function(density, xabs, accept, endpoints=c(-Inf, Inf)){
    	   xabs[k+1] <- x_star
    	   xabs <- sort(xabs)
    	 }
-   	 
-   	 ###Test for concavity before moving on to the next sample round
-   	 #Send xabs to the the h_prime function
-   	 concave <- h_prime(xabs)
-   	 runs <- (length(rle(sign(concave))$lengths)) # for concavity there should only be two runs and one switch in the runs
-   	 if(runs>2){
-   	   print("The log density function is not concave.")
-   	   break}
-   	 
   	}
   }
   	#------End Sampling-----------------------------
@@ -183,13 +183,20 @@ return(sample)
 #------End ARS Scheme-----------------------------
 
 #-----Begin Testing Scheme--------------------------
-ARS_testing <- function(sample,density){
+ARS_testing <- function(alpha=0.001){
  
-  ##user visual plot for testing
+  #Standard Normal Tests
+  density <- quote((1/sqrt(2*pi))*exp((-x^2)/2))
+  xabs <- c(-1,1) #user-specified starting abcissae
+  accept <- 1000 #total number of points required to accept
+  sample <- ARS(density, xabs, accept)
   
+  print("Beginning tests with Standard Normal")
+  
+  ##user visual plot for testing
   par(mfrow=c(2,1))
   #True density
-  x=seq(min(sample),max(sample),by=0.01)
+  x <- seq(min(sample),max(sample),by=0.01)
   plot(x,eval(density),type="l",xlab="",ylab="",main="True Density")
   #Histogram of a sample from our function
   hist(sample, freq=F, main="Histogram of ARS sample", xlab="",ylab="")
@@ -198,20 +205,50 @@ ARS_testing <- function(sample,density){
     print("User determined that plots do not match. Terminating testing algorithm.")
     stop(call. = FALSE)
   }
-  else{
-    print("User determined that plots do match")
-    print("Beginning Kolmogorov-Smirnov test")
   
-    ###Kolmogorov-Smirnov test for equality of two cdf distributions
-    KS <- ks.test(sample, "pnorm", 3, 2) # two-sided, exact
-    par(mfrow=c(1,1))
-    plot(ecdf(sample),lty=1, col="red", lwd=0.5, main="CDF Comparison Plot")
-    lines(x, pnorm(x, mean = 0, sd = 1, lower.tail = TRUE, log.p = FALSE),lty=2,col="blue", lwd=1.25)
-    legend("bottomright", legend=c("Empirical CDF of ARS Sample", "Standard Normal CDF"), cex=0.3, col=c("red","blue"), lty=c(1, 2), lwd=c(0.5, 1.25))
-    ###Report the pvalue
-    if(KS[[2]]<0.001){print("P-value < 0.001. K-S test passed.")}
-    else{print("P-value does not indicate statistical equality between the distributions")}
-    print(KS)
-}
+  print("User determined that plots do match")
+  print("Beginning Kolmogorov-Smirnov test")
+
+  ###Kolmogorov-Smirnov test for equality of two cdf distributions
+  KS <- ks.test(sample, "pnorm", 3, 2) # two-sided, exact
+  par(mfrow=c(1,1))
+  plot(ecdf(sample),lty=1, col="red", lwd=0.5, main="CDF Comparison Plot")
+  lines(x, pnorm(x, mean = 0, sd = 1, lower.tail = TRUE, log.p = FALSE),lty=2,col="blue", lwd=1.25)
+  legend("bottomright", legend=c("Empirical CDF of ARS Sample", "Standard Normal CDF"), cex=0.5, col=c("red","blue"), lty=c(1, 2), lwd=c(0.5, 1.25))
+  ###Report the pvalue
+  if(KS[[2]]<alpha){
+    print(paste("P-value = ", KS[[2]]," < ", alpha, ". K-S test passed.", sep=""))
+  } else{print(paste("P-value = ", KS[[2]]," !< ", alpha, ". P-value does not indicate statistical equality between the distributions. K-S test failed.", sep=""))}
+
+  ###Pass a non-log concave density Pareto(2,3)
+  print("Beginning test for non-log concave density.")
+  density <- quote((7*1^7)/x^(7+1))
+  xabs <- c(2, 5)
+  endpoints <- c(1,Inf)
+  sample <- ARS(density, xabs, accept, endpoints)
+  if (sample!="The density function is not logarithmically concave.  Terminating ARS algorithm."){
+    print("ARS algorithm did not detect non-log concave Pareto(2,3) density.  Test failed.  Terminating testing algorithm.")
+    stop(call. = FALSE)
+  }
+  print("ARS algorithm detected non-log concave Pareto(2,3) density.  Non-log concave test passed."  )
+  
+  ##Pass a Beta(2,2) density a first starting abcissae<0
+  print("Beginning test for an initial abcessa outside of defined density domain.")
+  density <- quote(6*x*(1-x))
+  xabs <- c(-2,0.7)
+  accept <- 1000 
+  endpoints <- c(0,1)
+  sample <- ARS(density,xabs,accept,endpoints)
+  if (sample!="Initial abcissae are not in the density's specified domain.  Terminating ARS algorithm."){
+    print("ARS algorithm did not detect that an initial abcissa is outside of the defined density domain.  Test failed.  Terminating testing algorithm.")
+    stop(call. = FALSE)
+  }
+  print("ARS algorithm detected initial abcessa outside of domain.  Initial abcessa domain test passed."  )
+
 }
 #-----End Testing Scheme-----------------------------
+
+
+#------Run Testing Scheme------------------------
+ARS_testing()
+#------Complete Run of Testing Scheme------------------------
